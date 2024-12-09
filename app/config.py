2ad08a43e1838b4a228c65bb632e9249dff86c36
Yerasyl0405 <saparbeke04@mail.ru>
2024-12-09 14:11:55 +0500

    fd

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', '4543ergdfg')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://pm_project_user:bQpkG3Be0mz48gOTMrcHCWyOJDKShCcL@dpg-ct88oqogph6c73d3tnpg-a.oregon-postgres.render.com/pm_project'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://pm_project_user:bQpkG3Be0mz48gOTMrcHCWyOJDKShCcL@dpg-ct88oqogph6c73d3tnpg-a.oregon-postgres.render.com/pm_project?sslmode=disable'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.gmail.com'  # Example for Gmail, adjust for your email provider
     MAIL_PORT = 587
